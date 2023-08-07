b7429ac133230062f50717add81ed7ba345b574e
Nestor Debiesme <nesdebie@mi-r1-p3.s19.be>
2023-08-07 15:27:16 +0200

    beta mode of echo actually works but leaks

--- src/builtins_and_exec/builtins/ft_echo.c
+++ src/builtins_and_exec/builtins/ft_echo.c
@@ -6,7 +6,7 @@
 /*   By: nesdebie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 12:01:23 by nesdebie          #+#    #+#             */
-/*   Updated: 2023/08/07 12:09:44 by nesdebie         ###   ########.fr       */
+/*   Updated: 2023/08/07 15:22:45 by nesdebie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,6 +46,46 @@ static int	ft_add_flag(char **args)
 	return (c);
 }
 
+static void	ft_parse_and_write(t_shell *d, char *s)
+{
+	int i;
+	int j;
+	char *tmp;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == '$')
+		{
+			j = i + 1;
+			while (s[i + j] && s[i + j] != '$')
+				j++;
+			tmp = ft_substr(s, i + 1, j - 1);
+			if (!tmp)
+				return ;
+			if (tmp[0] == '?')
+			{
+				ft_putnbr_fd(d->exit_code, STDOUT_FILENO);
+				if (tmp[1] == '?')
+					tmp = ft_substr(tmp, i + 2, j);
+				else 
+					tmp = ft_substr(tmp, i + 1, j);
+			}
+			if (ft_getenv(d->envp_list, tmp))
+				ft_putstr_fd(ft_getenv(d->envp_list, tmp), STDOUT_FILENO);
+			else
+				ft_putstr_fd(tmp, STDOUT_FILENO);
+			i = j;
+			free(tmp);
+		}
+		else
+		{
+			ft_putchar_fd(s[i], STDOUT_FILENO);
+			i++;
+		}
+	}
+}
+
 void	ft_echo(t_shell *d, int num_cmd)
 {
 	int	i;
@@ -58,9 +98,14 @@ void	ft_echo(t_shell *d, int num_cmd)
 	while (d->cmd[num_cmd].args[i])
 	{
 		if (ft_strlen(d->cmd[num_cmd].args[i]))
-			ft_putstr_fd(d->cmd[num_cmd].args[i], STDOUT_FILENO);
-		//if (!flag)
-		//	ft_putchar_fd(' ', STDOUT_FILENO);
+		{
+			if (ft_strnstr(d->cmd[num_cmd].args[i], "$?", ft_strlen(d->cmd[num_cmd].args[i])))
+				ft_parse_and_write(d, d->cmd[num_cmd].args[i]);
+			else
+				ft_putstr_fd(d->cmd[num_cmd].args[i], STDOUT_FILENO);
+			if (d->cmd[num_cmd].args[i + 1])
+				ft_putchar_fd(' ', STDOUT_FILENO);
+		}
 		i++;
 	}
 	if (!flag)
