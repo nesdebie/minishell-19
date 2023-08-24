860e851d623411ec1b8293535d91a0bbaa704ec8
R0uge_ <merlinbourgeois@gmail.com>
2023-08-24 13:55:48 +0200

    correction token parsing

--- src/builtins_and_exec/builtins/ft_env.c
+++ src/builtins_and_exec/builtins/ft_env.c
@@ -6,7 +6,7 @@
 /*   By: mebourge <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 12:01:52 by nesdebie          #+#    #+#             */
-/*   Updated: 2023/08/24 12:55:47 by mebourge         ###   ########.fr       */
+/*   Updated: 2023/08/24 13:55:16 by mebourge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ void	ft_read_lst(void *lst)
 	t_env	*ls_env_node;
 
 	ls_env_node = (t_env *)(lst);
-	if (ls_env_node->name && ls_env_node->value && ft_strlen(ls_env_node->value))
+	if (ls_env_node->name && ls_env_node->value
+		&& ft_strlen(ls_env_node->value))
 	{
 		ft_putstr_fd(ls_env_node->name, STDOUT_FILENO);
 		ft_putchar_fd('=', STDOUT_FILENO);
